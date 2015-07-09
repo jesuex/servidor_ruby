f88352f40d9299c45f3316af1917c87fb6b76180
jesuex <jesuelucasdiogo@gmail.com>
2015-07-09 18:40:43 -0300

    Update servidor_central.rb
    Show de bola

--- servidor_central.rb
+++ servidor_central.rb
@@ -1,8 +1,16 @@
+#Código de um servidor central para inserção de servidores locais.
+#Registro de servidores apenas com Domínio e IP
+
+
+#Solicitação de uso de socket
 require 'socket'
 require 'rdbi-driver-sqlite3'
- 
+
+#Ultilizando protocolo UDP para abrir uma porta ou conexão 
 socket = UDPSocket.new
 socket.bind("", 2100)
+
+#chamando bibliotecas para conxão com banco de dados
 dbh = RDBI.connect(:SQLite3, :database => "servidor_dominio.db")
 aspas = '"'
 reply = nil
@@ -10,6 +18,9 @@ loop {
   puts "Conectado"
   s, sender = socket.recvfrom(1024)
   puts s
+
+#Tratando a solicitação, onde a solicitação [0] ficara com a mensagem REG, solicitação [1] ficará com o nome de domínio
+#e solicitação [2] ficará com o endereço de IP
   solicitacao = s.split
   cliente_ip = sender[3]
   cliente_port = sender[1]
@@ -17,6 +28,8 @@ loop {
     if solicitacao[1] != nil && solicitacao[2] != nil
       begin
         puts "RECEBENDO SOLICITACAO DE REGISTRO DE DOMINIO"
+
+#Chamando comando SQL para cadastrar servidores.    
     dbh.execute("insert into servidores (dominio, ip) values ( \"#{solicitacao[1]}\", \"#{solicitacao[2]}\")")
     puts " Registro Realizado com Sucesso!"
     socket.send "REGOK", 0 , cliente_ip, cliente_port
